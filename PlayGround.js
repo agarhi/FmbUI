5d57ef0238f0903588b808aac977d8f27749efd5
agarhi <garhiasif@gmail.com>
2023-11-19 20:10:04 -0800

    Added Next week, prev week functionalities

--- PlayGround.js
+++ PlayGround.js
@@ -5,6 +5,7 @@ import { View, Text, StyleSheet, TouchableOpacity, TextInput, CheckBox, ScrollVi
 import { useState, useEffect } from 'react';
 import Moment from 'moment';
 import integrate from './integration';
+import { Icon } from 'react-native-elements'
 
 
 const PlayGround = ({ navigation }) => {
@@ -12,11 +13,27 @@ const PlayGround = ({ navigation }) => {
   const [isDataAvailable, setDataAvailable] = React.useState(false)
   const [menuDataLocal, setMenuDataLocal] = useState([])
   const [niyaz, setNiyaz] = useState(false);
+  const date = new Date()
+  const [currDate, setCurrDate] = useState(date);
+
   const onDismiss = React.useCallback(() => {
     setVisible(false)
   }, [setVisible])
 
+  const nextWeekFrom = (date, nextOrPrev) => {
+    var time = date.getTime();
+    var newDate = new Date();
+    time = time + (nextOrPrev * 7 * 24 * 60 * 60 * 1000);
+    newDate.setTime(time);
+    setCurrDate(newDate)
+    return newDate
+  }
+
   const onChange = async ({ date }) => {
+    fetch(date)
+  }
+
+  const fetch =  async (date) => {
     setVisible(false)
     const dateFormat = 'YYYY-MM-DD'
     var selected_date = Moment(date).format(dateFormat);
@@ -34,7 +51,8 @@ const PlayGround = ({ navigation }) => {
      }) */
     setDataAvailable(true)
   }
-  const date = new Date()
+
+  
 
   const dayOf = (date) => {
     return Moment(date).format('ddd');
@@ -52,40 +70,50 @@ const PlayGround = ({ navigation }) => {
 
   return (
     <ScrollView>
-    <>
-      <DatePickerModal
-        mode="single"
-        visible={visible}
-        onDismiss={onDismiss}
-        date={date}
-        onConfirm={onChange}
-        saveLabel="Save" // optional
-        label="Select date" // optional
-        animationType="slide" // optional, default is 'slide' on ios/android and 'none' on web
-        locale='en-US'
-      />
-      <Button onPress={() => setVisible(true)}>
-        Select date
-      </Button>
+      <>
+        <DatePickerModal
+          mode="single"
+          visible={visible}
+          onDismiss={onDismiss}
+          date={currDate}
+          onConfirm={onChange}
+          saveLabel="Save" // optional
+          label="Select date" // optional
+          animationType="slide" // optional, default is 'slide' on ios/android and 'none' on web
+          locale='en-US'
+        />
+        <Button onPress={() => setVisible(true)}>
+          Select date
+        </Button>
 
-      {
-        isDataAvailable ? (
-          <View style={styles.container}>
-            <View style={styles.data}>
-              <View style={styles.date}><Text style={styles.header}>Date</Text></View>
-              <View style={styles.textCenter} ><Text style={styles.header}>Item</Text></View>
-              <View style={styles.check}><Text style={styles.header}>Niyaz</Text></View>
+        {
+          isDataAvailable ? (
+            <View style={styles.container}>
+              <View style={styles.data}>
+                <View style={styles.date}><Text style={styles.header}>Date</Text></View>
+                <View style={styles.textCenter} ><Text style={styles.header}>Item</Text></View>
+                <View style={styles.check}><Text style={styles.header}>Niyaz</Text></View>
+              </View>
+              {dataListArr}
+              <View style={{ flexDirection: 'row', borderWidth: 0, justifyContent: 'center', marginTop: 10 }}>
+                <TouchableOpacity style={styles.buttonTO}>
+                  <Text style={{ color: 'white', width: 70, textAlign: 'center' }}>Submit</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ flexDirection: 'row', borderWidth: 0, justifyContent: 'center', marginTop: 10 }}>
+              <TouchableOpacity style={{ flexDirection: 'row', marginRight:20 }} onPress={() => fetch(nextWeekFrom(currDate, -1))}>
+                  <Icon name="arrow-left" size={20} />
+                  <Text style={styles.nextPrev}>Prev week</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ flexDirection: 'row', marginLeft:20 }} onPress={() => fetch(nextWeekFrom(currDate, 1))}>
+                  <Text style={styles.nextPrev}>Next week</Text>
+                  <Icon name="arrow-right" size={20} />
+                </TouchableOpacity>
+              </View>
             </View>
-            {dataListArr}
-            <View style={{ flexDirection: 'row', borderWidth: 0, justifyContent: 'center', marginTop: 10 }}>
-              <TouchableOpacity style={styles.buttonTO}>
-                <Text style={{ color: 'white', width: 70, textAlign: 'center' }}>Submit</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        ) : (<View />)
-      }
-    </>
+          ) : (<View />)
+        }
+      </>
     </ScrollView>)
 }
 
